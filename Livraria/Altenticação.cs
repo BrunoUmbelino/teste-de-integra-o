0a2935dfc421b7684877ef614bd576c23d2363e6
Bruno Umbelino <bruno.umbelino@lambda3.com.br>
2022-05-16 16:31:15 -0300

    first commit

---
+++ Livraria/Altenticação.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public class Altenticação
+    {
+        public string Email { get; private set; }
+
+        public bool Logado { get; set; }    
+
+        public Altenticação(string email, string Senha)
+        {
+            if (email == "maria@email.com" && Senha == "123456")
+            {
+                this.Email = email;
+                this.Logado = true;
+            }
+            else
+            {
+                this.Email = "";
+                this.Logado = false;
+            }
+        }
+    }
+}
---
+++ Livraria/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Livraria;
+
+var Altenticação = new Altenticação("maria@email.com", "123456");
+Console.WriteLine(Altenticação.Logado);
